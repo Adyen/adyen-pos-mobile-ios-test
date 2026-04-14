2a033c315cd91a9a976ed4a71c810721ef8df63a
James <17139737+mcguire-james@users.noreply.github.com>
2026-04-14 14:08:37 +0200

    Release 3.15.1

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOSTEST",
-            url: "https://pos-mobile-test.cdn.adyen.com/adyen-pos-ios/3.15.0/Test/AdyenPOS.xcframework.zip",
-            checksum: "87af3a84bf3c242528d5c42b6b042cd7e6b40395f04946df128e3380adacb381"
+            url: "https://pos-mobile-test.cdn.adyen.com/adyen-pos-ios/3.15.1/Test/AdyenPOS.xcframework.zip",
+            checksum: "dd2e5ab08741eaeaf5963942f8a0eeea762d2fc088068ee8e5936356d6e64298"
         ),
         .binaryTarget(
             name: "ADYPOSTEST",
-            url: "https://pos-mobile-test.cdn.adyen.com/adyen-pos-ios/3.15.0/Test/ADYPOS.xcframework.zip",
-            checksum: "2ad184ca0f04c4488a43a8cee9bacc4c95a12fa8beb9632fa6f60074bab23d6f"
+            url: "https://pos-mobile-test.cdn.adyen.com/adyen-pos-ios/3.15.1/Test/ADYPOS.xcframework.zip",
+            checksum: "eb07c53d737394aedad5a4808d2905548e9b1c8d27a6720d7b3fa075cdfa3812"
         )
     ]
 )
